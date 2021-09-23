718055d2cd28dd801112b41a349b21f044ea8517
student12-Lvova <91256538+student12-Lvova@users.noreply.github.com>
2021-09-23 15:33:43 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,28 +30,29 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dgvTableThing = new System.Windows.Forms.DataGridView();
-            this.pADD = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.bADD = new System.Windows.Forms.Button();
             this.bDELETE = new System.Windows.Forms.Button();
+            this.pADD = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.bADD = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvTableThing = new System.Windows.Forms.DataGridView();
             this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Name_thing = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Proizvodstvo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvTableThing)).BeginInit();
             this.pADD.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTableThing)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -66,6 +67,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.bDELETE);
             this.tabPage1.Controls.Add(this.pADD);
             this.tabPage1.Controls.Add(this.dgvTableThing);
@@ -77,28 +79,14 @@
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // bDELETE
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 409);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // dgvTableThing
-            // 
-            this.dgvTableThing.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTableThing.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Type,
-            this.Name_thing,
-            this.Price,
-            this.Proizvodstvo});
-            this.dgvTableThing.Location = new System.Drawing.Point(6, 3);
-            this.dgvTableThing.Name = "dgvTableThing";
-            this.dgvTableThing.Size = new System.Drawing.Size(446, 400);
-            this.dgvTableThing.TabIndex = 0;
+            this.bDELETE.Location = new System.Drawing.Point(639, 251);
+            this.bDELETE.Name = "bDELETE";
+            this.bDELETE.Size = new System.Drawing.Size(75, 23);
+            this.bDELETE.TabIndex = 2;
+            this.bDELETE.Text = "Удалить";
+            this.bDELETE.UseVisualStyleBackColor = true;
             // 
             // pADD
             // 
@@ -117,63 +105,24 @@
             this.pADD.Size = new System.Drawing.Size(212, 215);
             this.pADD.TabIndex = 1;
             // 
-            // label1
+            // checkBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Добавление товара";
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(142, 129);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // bADD
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 43);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(105, 69);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(105, 95);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(5, 76);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Наименование";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(5, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Цена";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(5, 129);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Производится";
+            this.bADD.Location = new System.Drawing.Point(65, 189);
+            this.bADD.Name = "bADD";
+            this.bADD.Size = new System.Drawing.Size(75, 23);
+            this.bADD.TabIndex = 11;
+            this.bADD.Text = "Добавить";
+            this.bADD.UseVisualStyleBackColor = true;
+            this.bADD.Click += new System.EventHandler(this.bADD_Click);
             // 
             // label6
             // 
@@ -184,23 +133,75 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "Тип товара";
             // 
-            // bADD
+            // label5
             // 
-            this.bADD.Location = new System.Drawing.Point(65, 189);
-            this.bADD.Name = "bADD";
-            this.bADD.Size = new System.Drawing.Size(75, 23);
-            this.bADD.TabIndex = 11;
-            this.bADD.Text = "Добавить";
-            this.bADD.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Производится";
             // 
-            // bDELETE
+            // label4
             // 
-            this.bDELETE.Location = new System.Drawing.Point(639, 251);
-            this.bDELETE.Name = "bDELETE";
-            this.bDELETE.Size = new System.Drawing.Size(75, 23);
-            this.bDELETE.TabIndex = 2;
-            this.bDELETE.Text = "Удалить";
-            this.bDELETE.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Цена";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Наименование";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление товара";
+            // 
+            // dgvTableThing
+            // 
+            this.dgvTableThing.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTableThing.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Type,
+            this.Name_thing,
+            this.Price,
+            this.Proizvodstvo});
+            this.dgvTableThing.Location = new System.Drawing.Point(6, 3);
+            this.dgvTableThing.Name = "dgvTableThing";
+            this.dgvTableThing.Size = new System.Drawing.Size(446, 400);
+            this.dgvTableThing.TabIndex = 0;
             // 
             // Type
             // 
@@ -224,14 +225,25 @@
             this.Proizvodstvo.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Proizvodstvo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
-            // checkBox1
+            // tabPage2
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(142, 129);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 12;
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 409);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(582, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -243,9 +255,9 @@
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvTableThing)).EndInit();
             this.pADD.ResumeLayout(false);
             this.pADD.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTableThing)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -272,6 +284,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Name_thing;
         private System.Windows.Forms.DataGridViewTextBoxColumn Price;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Proizvodstvo;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,38 @@ namespace LB1
         public Form1()
         {
             InitializeComponent();
+
+           
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void bADD_Click(object sender, EventArgs e)
         {
+            StreamWriter sw = new StreamWriter(@"C:\Львова\ЛБ2_КРПП\Price-list.txt");
+            if (checkBox1.Checked)
+            {
+                sw.WriteLine($"{textBox1}.{textBox2}.{textBox3}.да");
+            }
+            else sw.WriteLine($"{textBox1}.{textBox2}.{textBox3}.нет");
+            sw.Close();
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader fs = new StreamReader(@"C:\Львова\ЛБ2_КРПП\Price-list.txt", Encoding.Default);
+            string s = fs.ReadToEnd();
+            string[] things = s.Split('|');
+            for (int i = 0; i < things.Length; i++)
+            {
+                string[] inf = things[i].Split('.');
+                if (inf[3] == "да")
+                {
+                    dgvTableThing.Rows.Add(inf[0], inf[1], inf[2], true);
+                }
+
+                else
+                    dgvTableThing.Rows.Add(inf[0], inf[1], inf[2], false);
+            }
+            fs.Close();
         }
     }
 }
